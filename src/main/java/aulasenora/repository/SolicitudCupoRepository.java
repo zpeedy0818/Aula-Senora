4ddb20314b01cc628e08422091b42255089898ed
zpeedy0818 <tuemail@ejemplo.comocadavid0818@hotmail.com>
2026-04-28 08:50:23 -0500

    DEV-25: Entidades de Base de Datos para Horarios y Solicitudes

---
+++ src/main/java/aulasenora/repository/SolicitudCupoRepository.java
@@ -0,0 +1,16 @@
+package aulasenora.repository;
+
+import aulasenora.model.HorarioDisponible;
+import aulasenora.model.SolicitudCupo;
+import aulasenora.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SolicitudCupoRepository extends JpaRepository<SolicitudCupo, Long> {
+    List<SolicitudCupo> findByEstudiante(Usuario estudiante);
+    List<SolicitudCupo> findByHorario(HorarioDisponible horario);
+    List<SolicitudCupo> findByHorarioIn(List<HorarioDisponible> horarios);
+}
